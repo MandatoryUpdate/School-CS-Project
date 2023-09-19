eb641c7e49709a099ebf7d50ea18a4a57d1bf390
MandatoryUpdate <118033042+MandatoryUpdate@users.noreply.github.com>
2023-09-19 14:56:03 -0500

    Add files via upload

---
+++ Lab0StarterCode/Lab0Runner.java
@@ -0,0 +1,15 @@
+package Lab0StarterCode;
+
+//name:
+
+public class Lab0Runner {
+
+
+    public static void main(String[] args)
+    {
+        Lab0 lab = new Lab0();
+        lab.display (1, 100);
+    }
+
+    
+}
